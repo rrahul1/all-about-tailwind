bb45dd265f76b25b26917e4f5515b57360580008
Rahul Singh <rathore.rahul.1999@gmail.com>
2024-05-07 22:11:38 +0530

    initial push

---
+++ 01-tailwind-project/src/App.jsx
@@ -0,0 +1,17 @@
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+   return (
+      <>
+         <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16">
+            <Header />
+            <Body />
+            <Footer />
+         </div>
+      </>
+   );
+}
+
+export default App;
